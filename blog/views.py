37951479c771870ddb19189ccbb7ead786f4eb79
HarmanGill <harman97gill@gmail.com>
2024-06-18 20:44:09 -0400

    deployment

---
+++ blog/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from .models import Blog
+from rest_framework import viewsets, permissions
+from .serializers import BlogSerializer
+
+class BlogViewSet(viewsets.ModelViewSet):
+  queryset = Blog.objects.all()
+  serializer_class = BlogSerializer
+  permission_classes = [permissions.AllowAny]
+
+# Create your views here.
